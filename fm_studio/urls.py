2e9e8d045ffe6f18a276f61deb0a3b0e939a3fb9
[Yernar] <toktaryernar@gmail.com>
2021-10-16 13:51:03 +0600

    feat: media config

--- fm_studio/urls.py
+++ fm_studio/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 from django.conf.urls import url, include
 from django.views.static import serve
-from .settings import MEDIA_ROOT
+from fm_studio import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,5 +11,5 @@ urlpatterns = [
     url(r'^user/', include('user.urls', namespace='user')),
     url(r'^', include('music.urls', namespace='music')),
 
-    url(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve, {"document_root": settings.MEDIA_ROOT}),
 ]
