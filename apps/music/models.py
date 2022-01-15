59309c2c685d850731bf2e6470688fd7b4872876
YernarT <toktaryernar@gmail.com>
2022-01-15 23:32:40 +0600

    update: 添加 create_time字段

--- apps/music/models.py
+++ apps/music/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class MusicType(models.Model):
     name = models.CharField(max_length=40, verbose_name='жанр атауы')
-    
+
     class Meta:
         db_table = 'music_type'
         verbose_name = 'музыка жанр'
@@ -16,10 +16,11 @@ class MusicType(models.Model):
 class Music(models.Model):
     name = models.CharField(max_length=40, verbose_name='музыка атауы')
     music = models.FileField(upload_to='audio/', verbose_name='музыка файлы')
-    music_type = models.ManyToManyField(to='MusicType', related_name='music_types', verbose_name='мазыка жанры')
-    like = models.IntegerField(default=0, verbose_name='лайк саны')
+    music_type = models.ManyToManyField(
+        to='MusicType', related_name='music_types', verbose_name='мазыка жанры')
     views = models.IntegerField(default=0, verbose_name='есту саны')
-
+    create_time = models.DateTimeField(
+        auto_now_add=True, verbose_name='құрылған уақыт')
 
     class Meta:
         db_table = 'music'
@@ -30,13 +31,30 @@ class Music(models.Model):
         return f'{self.name}'
 
 
-class Comment(models.Model):
-    content = models.CharField(max_length=255, verbose_name='пікір')
-    music = models.ForeignKey(to='Music', on_delete=models.CASCADE, verbose_name='музыка')
-    user = models.ForeignKey(to='user.User', on_delete=models.CASCADE, verbose_name='пайдаланушы')
+class MusicLike(models.Model):
+    music = models.ForeignKey(
+        to='Music', on_delete=models.CASCADE, verbose_name='музыка')
+    user = models.ForeignKey(
+        to='user.User', on_delete=models.CASCADE, verbose_name='пайдаланушы')
 
     class Meta:
-        db_table = 'comment'
+        db_table = 'music_like'
+        verbose_name = 'лайк'
+        verbose_name_plural = 'лайктар'
+
+    def __str__(self):
+        return f'{self.user}'
+
+
+class MusicComment(models.Model):
+    content = models.CharField(max_length=255, verbose_name='пікір')
+    music = models.ForeignKey(
+        to='Music', on_delete=models.CASCADE, verbose_name='музыка')
+    user = models.ForeignKey(
+        to='user.User', on_delete=models.CASCADE, verbose_name='пайдаланушы')
+
+    class Meta:
+        db_table = 'music_comment'
         verbose_name = 'пікір'
         verbose_name_plural = 'пікірлер'
 
