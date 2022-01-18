7953cb1d0463202af848818ddc6495a3fccf362e
YernarT <toktaryernar@gmail.com>
2022-01-18 22:21:45 +0600

    feat: 基础页面

---
+++ frontend/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+	<HomePage />
+</template>
+
+<script setup lang="ts">
+import './assets/style/reset.css';
+import './assets/style/customize.css';
+
+import HomePage from './pages/user/Home.vue';
+</script>
---
+++ frontend/src/pages/auth/Register.vue
@@ -0,0 +1,7 @@
+<template>
+	<h1 class="register-page">Register Page</h1>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
