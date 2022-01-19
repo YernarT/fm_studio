6bb5f803c396817d4a0a5691184419151153d574
YernarT <toktaryernar@gmail.com>
2022-01-19 23:08:22 +0600

    update: router-view渲染, 布局容器

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,29 @@
 <template>
-	<HomePage />
+	<div class="layout-container">
+		<router-view></router-view>
+	</div>
 </template>
 
-<script setup lang="ts">
-import './assets/style/reset.css';
-import './assets/style/customize.css';
+<script setup lang="ts"></script>
 
-import HomePage from './pages/user/Home.vue';
-</script>
+<!-- private style -->
+<style scoped lang="less">
+.layout-container {
+	width: 100%;
+	height: 100vh;
+	overflow: hidden;
+	background-color: @backgroundColor;
+}
+</style>
+
+<!-- public style -->
+<style lang="less">
+.container {
+	margin: auto;
+	width: 100%;
+
+	@media screen and (min-width: 980px) {
+		width: 980px;
+	}
+}
+</style>
