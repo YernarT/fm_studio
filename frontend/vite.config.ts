0d4dfa0b117373d6a494c304bb08665afac65fd1
YernarT <toktaryernar@gmail.com>
2022-01-19 22:38:59 +0600

    feat: 添加 alias, less 支持

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+
+const pathResolve = (dir: string) => resolve(__dirname, dir);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	server: {
+		host: '0.0.0.0',
+		open: true,
+	},
 	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': pathResolve('./src'),
+			'pages': pathResolve('./src/pages'),
+			'components': pathResolve('./src/components'),
+			'assets': pathResolve('./src/assets'),
+			'store': pathResolve('./src/store'),
+			'configs': pathResolve('./src/configs'),
+		},
+	},
+	css: {
+		preprocessorOptions: {
+			less: {
+				javascriptEnabled: true,
+				additionalData: `@import "${pathResolve(
+					'src/assets/style/variables.less',
+				)}";`,
+			},
+		},
+	},
 });
