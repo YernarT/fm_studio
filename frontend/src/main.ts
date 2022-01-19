ac89d811b34140ae02703000974047d67ae2c4a4
YernarT <toktaryernar@gmail.com>
2022-01-19 22:54:57 +0600

    update: use 路由, vuex

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,19 @@
 import { createApp } from 'vue';
+
+// Arco UI
 import ArcoVue from '@arco-design/web-vue';
 import '@arco-design/web-vue/dist/arco.css';
+
 import App from './App.vue';
+import router from './routes';
+import store from 'store/index';
+
+// Common style
+import 'assets/style/reset.css';
+import 'assets/style/customize.less';
 
 const app = createApp(App);
 
-app.use(ArcoVue);
+app.use(ArcoVue).use(store).use(router);
 
 app.mount('#app');
