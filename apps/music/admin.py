3ce28e59541bbba28169d39badd598b5a4b9cf5d
YernarT <toktaryernar@gmail.com>
2022-01-19 22:37:36 +0600

    feat: add album, favorites model

--- apps/music/admin.py
+++ apps/music/admin.py
@@ -4,4 +4,9 @@ from .models import *
 admin.site.register(Music)
 admin.site.register(MusicType)
 admin.site.register(MusicLike)
-admin.site.register(MusicComment)
+
+admin.site.register(Album)
+admin.site.register(AlbumComment)
+
+admin.site.register(Favorites)
+admin.site.register(FavoriteMusic)
--- apps/music/models.py
+++ apps/music/models.py
@@ -1,5 +1,36 @@
 from django.db import models
-from user.models import User
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=40, verbose_name='альбом атауы')
+    cover = models.ImageField(upload_to='img/music/album-cover/',
+                              null=True, blank=True, verbose_name='альбом суреті')
+    author = models.ForeignKey(
+        'user.User', on_delete=models.SET_NULL, null=True, verbose_name='пайдаланушы')
+
+    class Meta:
+        db_table = 'music_album'
+        verbose_name = 'альбом'
+        verbose_name_plural = 'альбомдар'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class AlbumComment(models.Model):
+    content = models.CharField(max_length=255, verbose_name='пікір')
+    album = models.ForeignKey(
+        to='Album', on_delete=models.CASCADE, verbose_name='альбом')
+    user = models.ForeignKey(
+        to='user.User', on_delete=models.CASCADE, verbose_name='пайдаланушы')
+
+    class Meta:
+        db_table = 'album_comment'
+        verbose_name = 'пікір'
+        verbose_name_plural = 'пікірлер'
+
+    def __str__(self):
+        return f'{self.content}'
 
 
 class MusicType(models.Model):
@@ -20,7 +51,7 @@ class Music(models.Model):
     music_type = models.ManyToManyField(
         to='MusicType', related_name='music_types', verbose_name='мазыка жанры')
     auhtor = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True, verbose_name='авторы')
+        'user.User', on_delete=models.SET_NULL, null=True, verbose_name='авторы')
     views = models.IntegerField(default=0, verbose_name='есту саны')
     create_time = models.DateTimeField(
         auto_now_add=True, verbose_name='құрылған уақыт')
@@ -49,17 +80,32 @@ class MusicLike(models.Model):
         return f'{self.user}'
 
 
-class MusicComment(models.Model):
-    content = models.CharField(max_length=255, verbose_name='пікір')
-    music = models.ForeignKey(
-        to='Music', on_delete=models.CASCADE, verbose_name='музыка')
-    user = models.ForeignKey(
-        to='user.User', on_delete=models.CASCADE, verbose_name='пайдаланушы')
+class Favorites(models.Model):
+    name = models.CharField(max_length=40, verbose_name='таңдаулар атауы')
+    creator = models.ForeignKey(
+        'user.User', on_delete=models.CASCADE, verbose_name='құрушы')
+    create_time = models.DateTimeField(
+        auto_now_add=True, verbose_name='құрылған уақыт')
 
     class Meta:
-        db_table = 'music_comment'
-        verbose_name = 'пікір'
-        verbose_name_plural = 'пікірлер'
+        db_table = 'music_favorites'
+        verbose_name = 'таңдаулар'
+        verbose_name_plural = 'таңдаулар'
 
     def __str__(self):
-        return f'{self.content}'
+        return f'{self.name}'
+
+
+class FavoriteMusic(models.Model):
+    favorites = models.ForeignKey(
+        'Favorites', on_delete=models.CASCADE, verbose_name='таңдаулар')
+    music = models.ForeignKey(
+        to='Music', on_delete=models.CASCADE, verbose_name='музыка')
+
+    class Meta:
+        db_table = 'favorites_music'
+        verbose_name = 'таңдаулардағы музыка'
+        verbose_name_plural = 'таңдаулардағы музыкалар'
+
+    def __str__(self):
+        return f'{self.music.name}'
