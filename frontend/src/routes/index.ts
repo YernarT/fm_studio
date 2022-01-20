b3391d4c8f802576611dd1149081626d8fe7c72d
YernarT <toktaryernar@gmail.com>
2022-01-20 18:38:54 +0600

    update: 新增基础页面路由

--- frontend/src/routes/index.ts
+++ frontend/src/routes/index.ts
@@ -12,6 +12,41 @@ const routes: Array<any> = [
 			next();
 		},
 	},
+	{
+		path: '/my-music',
+		name: 'MyMusic',
+		auth: true,
+		component: () =>
+			import(/* webpackChunkName: "MyMusic" */ 'pages/user/MyMusic/index.vue'),
+		beforeEnter: (to: object, from: object, next: Function): any => {
+			console.log(to, from);
+			next();
+		},
+	},
+	{
+		path: '/leaderboard',
+		name: 'Leaderboard',
+		auth: false,
+		component: () =>
+			import(
+				/* webpackChunkName: "Leaderboard" */ 'pages/user/Leaderboard/index.vue'
+			),
+		beforeEnter: (to: object, from: object, next: Function): any => {
+			console.log(to, from);
+			next();
+		},
+	},
+	{
+		path: '/profile',
+		name: 'Profile',
+		auth: true,
+		component: () =>
+			import(/* webpackChunkName: "Profile" */ 'pages/user/Profile/index.vue'),
+		beforeEnter: (to: object, from: object, next: Function): any => {
+			console.log(to, from);
+			next();
+		},
+	},
 ];
 
 const router = createRouter({
