25225a994c4a4e64dac0537f7866132eccc6a444
YernarT <toktaryernar@gmail.com>
2022-02-01 18:39:50 +0600

    update: :hummer: 添加路由权限检测

--- frontend/src/routes/index.ts
+++ frontend/src/routes/index.ts
@@ -1,47 +1,53 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import type { RouteRecordRaw } from 'vue-router';
 
-const routes = [
+import { createRouter, createWebHistory } from 'vue-router';
+import { Message } from '@arco-design/web-vue';
+import { user } from '@/providers';
+
+const routes: Array<RouteRecordRaw> = [
 	{
 		path: '/',
 		name: 'Home',
-		auth: false,
 		component: () =>
 			import(/* webpackChunkName: "Home" */ '@/pages/user/Home/index.vue'),
-		// beforeEnter: (to, from, next) => {
-		// 	next();
-		// },
 	},
 	{
 		path: '/my-music',
 		name: 'MyMusic',
-		auth: true,
 		component: () =>
-			import(/* webpackChunkName: "MyMusic" */ '@/pages/user/MyMusic/index.vue'),
-		// beforeEnter: (to, from, next) => {
-		// 	next();
-		// },
+			import(
+				/* webpackChunkName: "MyMusic" */ '@/pages/user/MyMusic/index.vue'
+			),
+		beforeEnter: (_, __, next) => {
+			if (!user.token) {
+				Message.warning('авторизация қажет етеді');
+				return;
+			}
+			next();
+		},
 	},
 	{
 		path: '/leaderboard',
 		name: 'Leaderboard',
-		auth: false,
 		component: () =>
 			import(
 				/* webpackChunkName: "Leaderboard" */ '@/pages/user/Leaderboard/index.vue'
 			),
-		// beforeEnter: (to, from, next) => {
-		// 	next();
-		// },
 	},
 	{
 		path: '/profile',
 		name: 'Profile',
-		auth: true,
 		component: () =>
-			import(/* webpackChunkName: "Profile" */ '@/pages/user/Profile/index.vue'),
-		// beforeEnter: (to, from, next) => {
-		// 	next();
-		// },
+			import(
+				/* webpackChunkName: "Profile" */ '@/pages/user/Profile/index.vue'
+			),
+		beforeEnter: (_, __, next) => {
+			if (!user.token) {
+				Message.warning('авторизация қажет етеді');
+				return;
+			}
+			next();
+		},
 	},
 ];
 
