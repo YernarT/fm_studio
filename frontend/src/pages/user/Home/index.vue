afcc839121cd0abdff18d06a0a711a29d80710a2
YernarT <toktaryernar@gmail.com>
2022-02-03 21:50:38 +0600

    refactor: :recycle: 使用 BaseLayout 组件重写

--- frontend/src/pages/user/Home/index.vue
+++ frontend/src/pages/user/Home/index.vue
@@ -1,34 +1,7 @@
 <template>
-	<Navbar />
-	<div class="container home-page">
-		<div class="page-content">
-			<h1>home page</h1>
-		</div>
-	</div>
-	<AuthModal />
-	<!-- <MusicPlayer /> -->
-	<BottomNavigationBar />
+	<h1>主页</h1>
 </template>
 
-<script setup lang="ts">
-import Navbar from '@/components/user/Navbar/index.vue';
-import BottomNavigationBar from '@/components/user/BottomNavigationBar/index.vue';
-import AuthModal from '@/components/auth/AuthModal/index.vue';
-// import MusicPlayer from '@/components/user/MusicPlayer/index.vue';
-</script>
+<script setup lang="ts"></script>
 
-<style scoped lang="less">
-.home-page {
-	padding: 20px 10px 0 40px;
-	height: 100%;
-
-	@media screen and (max-width: 540px) {
-		padding: 0 10px;
-		height: calc(100% - 54px);
-	}
-
-	.page-content {
-		height: 100%;
-	}
-}
-</style>
+<style scoped lang="less"></style>
--- frontend/src/pages/user/MyMusic/index.vue
+++ frontend/src/pages/user/MyMusic/index.vue
@@ -1,32 +1,7 @@
 <template>
-	<Navbar />
-	<div class="container home-page">
-		<div class="page-content">
-			<h1>我的音乐</h1>
-		</div>
-	</div>
-	<!-- <MusicPlayer /> -->
-	<BottomNavigationBar />
+	<h1>我的音乐</h1>
 </template>
 
-<script setup lang="ts">
-import Navbar from '@/components/user/Navbar/index.vue';
-import BottomNavigationBar from '@/components/user/BottomNavigationBar/index.vue';
-import MusicPlayer from '@/components/user/MusicPlayer/index.vue';
-</script>
+<script setup lang="ts"></script>
 
-<style scoped lang="less">
-.home-page {
-	padding: 20px 10px 0 40px;
-	height: 100%;
-
-	@media screen and (max-width: 540px) {
-		padding: 0 10px;
-		height: calc(100% - 54px);
-	}
-
-	.page-content {
-		height: 100%;
-	}
-}
-</style>
+<style scoped lang="less"></style>
