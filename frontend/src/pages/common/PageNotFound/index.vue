3e8613dcb28fb40cd385f39c9885de4fc717248b
YernarT <toktaryernar@gmail.com>
2022-02-03 22:40:46 +0600

    feat: :sparkles: 新增 404 页面

---
+++ frontend/src/pages/common/PageNotFound/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<a-result status="404" subtitle="Ойбу.. табылмады">
+		<template #extra>
+			<a-space>
+				<a-button type="primary">Қайту</a-button>
+			</a-space>
+		</template>
+	</a-result>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang=""></style>
