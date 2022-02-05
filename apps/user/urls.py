24c55796c8ddf76f0bc06ea94a114ed16ed352d9
YernarT <toktaryernar@gmail.com>
2022-02-05 09:57:35 +0600

    feat: :sparkles: 新增修改用户信息接口

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -2,12 +2,10 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    # url(r'^$', UserInfoView.as_view(), name='user_info'),
-
     url(r'^login$', LoginView.as_view(), name='login'),
     url(r'^register$', RegisterView.as_view(), name='register'),
     url(r'^edit$', EditView.as_view(), name='edit'),
-
+    url(r'^edit/avatar$', EditAvatarView.as_view(), name='edit-avatar'),
 ]
 
 app_name = 'user'
