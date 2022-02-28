5c6596dad4b36ee595367c0bb4fb1b7a663516b6
YernarT <toktaryernar@gmail.com>
2022-02-28 11:05:08 +0600

    update: :hammer: 组件统一暴露

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,7 @@
+// auth
+export { default as AuthModal } from './auth/AuthModal/index.vue';
+
+// common
+export { default as BaseLayout } from './common/BaseLayout/index.vue';
+
+export { default as MusicCard } from './common/MusicCard/index.vue';
